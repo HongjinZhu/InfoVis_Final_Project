721fc8a2098e6a3f61774e38af073afb9daa8264
HongjinZhu <74286974+HongjinZhu@users.noreply.github.com>
2022-12-07 16:55:20 +0800

    Delete cell.js

--- cell.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export function Cell(props){
-    const { xScale, yScale, color, cause, year, mouseOver, mouseOut} = props;
-    return <g transform={`translate(${xScale(year)}, ${yScale(cause)})`}>
-        <rect width={xScale.bandwidth()} height={yScale.bandwidth()} fill={color} />
-    </g>
-}
