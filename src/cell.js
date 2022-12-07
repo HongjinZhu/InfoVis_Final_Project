fed1809193bd867d67fba36c8322c717772f662b
HongjinZhu <74286974+HongjinZhu@users.noreply.github.com>
2022-12-07 16:50:52 +0800

    Delete src directory

--- src/cell.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export function Cell(props){
-    const { xScale, yScale, color, cause, year} = props;
-    return <g transform={`translate(${xScale(cause)}, ${yScale(year)})`}>
-        <rect width={xScale.bandwidth()} height={yScale.bandwidth()} fill={color} />
-    </g>
-}
